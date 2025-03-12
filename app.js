d97c94ef4c27dca952b00f51d233ccf6cc486b30
Mathew Puckey <mathewpuckey@Mat-MacBook-Pro.local>
2025-03-12 17:13:52 +0000

    third time commit, added more comments to explain the code

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var printCount = 5;
 
+// The index parameter is the current loop iteration number passed from the for loop
 function printMessage(index) {
-    var msg = `Hello Falcon Punch ${index}!`;
+    const msg = `Hello Falcon Punch ${index}!`;
     console.log(msg);
 }
 
+// for loop, i starts at 0, loops as long as i is less than printCount var which is 5, each loop i increments up by 1
+//prints message each loop
 for(let i = 0; i < printCount; i++) {
     printMessage(i);
 }
